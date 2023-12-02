9c365d02704d54a6eae60b49546e0d3201739884
ammaarkhan <ammaarkhan03@gmail.com>
2023-12-01 17:13:23 -0800

    implement login logic

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,125 +1,159 @@
-import React from 'react';
-import { View, Text, Image, SafeAreaView, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import { useNavigation } from '@react-navigation/native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
+import db from "@react-native-firebase/database";
 
-export default function LoginScreen() {
+export default function LogInScreen() {
   const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    if (email && password) {
+      try {
+        await auth().signInWithEmailAndPassword(email, password);
+        navigation.push("Home"); // Replace with your home screen route
+      } catch (error) {
+        Alert.alert(
+          "Login Error",
+          error.message || "Please check your credentials"
+        );
+      }
+    } else {
+      Alert.alert("Input Required", "Please enter both email and password");
+    }
+  };
 
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
-      <Image style={styles.backgroundImage} source={require('../assets/backgroundimage.jpeg')} />
+      <Image
+        style={styles.backgroundImage}
+        source={require("../assets/backgroundimage.jpeg")}
+      />
 
-      
-    {/* title and form */}
-    <View style={styles.titleAndFormContainer}>
+      <View style={styles.titleAndFormContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.titleText}>Log In</Text>
+        </View>
 
-{/* title */}
-<View style={styles.titleContainer}>
-  <Text style={styles.titleText}>Login</Text>
-</View>
+        <View style={styles.formContainer}>
+          <View style={[styles.inputContainer, styles.mbEmail]}>
+            <TextInput
+              placeholder="Email"
+              placeholderTextColor={"gray"}
+              value={email}
+              onChangeText={setEmail} // Bind state setter
+            />
+          </View>
+          <View style={[styles.inputContainer, styles.mbPassword]}>
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={"gray"}
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword} // Bind state setter
+            />
+          </View>
 
-{/* form */}
-<View style={styles.formContainer}>
-  <View style={[styles.inputContainer, styles.mbEmail]}>
-    <TextInput
-      placeholder="Email"
-      placeholderTextColor={'gray'}
-    />
-  </View>
-  <View style={[styles.inputContainer, styles.mbPassword]}>
-    <TextInput
-      placeholder="Password"
-      placeholderTextColor={'gray'}
-      secureTextEntry
-    />
-  </View>
+          <View style={styles.fullWidth}>
+            <TouchableOpacity
+              style={styles.loginButton}
+              onPress={handleLogin} // Changed the onPress handler
+            >
+              <Text style={styles.loginButtonText}>Log In</Text>
+            </TouchableOpacity>
+          </View>
 
-  <View style={styles.fullWidth}>
-    <TouchableOpacity style={styles.loginButton} onPress={()=>navigation.push('Home')}>
-      <Text style={styles.loginButtonText}>Login</Text>
-    </TouchableOpacity>
-  </View>
-
-  <View style={styles.justifyCenter}>
-    <Text>Don't have an account? </Text>
-    <TouchableOpacity onPress={() => navigation.push('SignUp')}>
-      <Text style={styles.signupText}>SignUp</Text>
-    </TouchableOpacity>
-  </View>
-</View>
-</View>
-</View>
-);
+          <View style={styles.justifyCenter}>
+            <Text>Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.push("Register")}>
+              <Text style={styles.signupText}>Register</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-container: {
-flex: 1,
-backgroundColor: 'white',
-},
-backgroundImage: {
-...StyleSheet.absoluteFillObject,
-height: undefined,
-width: undefined,
-flex: 1,
-},
-titleAndFormContainer: {
-flex: 1,
-justifyContent: 'space-around',
-paddingTop: 40,
-paddingBottom: 10,
-},
-titleContainer: {
-flex: 1,
-alignItems: 'center',
-marginTop:20
-},
-titleText: {
-color: 'white',
-fontWeight: 'bold',
-fontSize: 28,
-letterSpacing: 1,
-},
-formContainer: {
-flex: 1,
-alignItems: 'center',
-marginHorizontal: 20, // Increase the margin for more space on each side
-marginBottom: 10,
-},
-inputContainer: {
-backgroundColor: 'rgba(0, 0, 0, 0.5)',
-padding: 10,
-borderRadius: 20,
-width: '100%',
-},
-mbEmail: {
-marginBottom: 20, // Increase this value for more space between email and the top
-},
-mbPassword: {
-marginBottom: 20, // Increase this value for more space between password and the email
-},
-fullWidth: {
-width: '100%',
-},
-loginButton: {
-backgroundColor: '#00BFFF',
-padding: 10,
-borderRadius: 20,
-marginBottom: 3,
-},
-loginButtonText: {
-fontSize: 18,
-fontWeight: 'bold',
-color: 'white',
-textAlign: 'center',
-},
-justifyCenter: {
-flexDirection: 'row',
-justifyContent: 'center',
-},
-signupText: {
-color: '#00BFFF',
-},
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  backgroundImage: {
+    ...StyleSheet.absoluteFillObject,
+    height: undefined,
+    width: undefined,
+    flex: 1,
+  },
+  titleAndFormContainer: {
+    flex: 1,
+    justifyContent: "space-around",
+    paddingTop: 40,
+    paddingBottom: 10,
+  },
+  titleContainer: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  titleText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 28,
+    letterSpacing: 1,
+  },
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    marginHorizontal: 20, // Increase the margin for more space on each side
+    marginBottom: 10,
+  },
+  inputContainer: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 10,
+    borderRadius: 20,
+    width: "100%",
+  },
+  mbEmail: {
+    marginBottom: 20, // Increase this value for more space between email and the top
+  },
+  mbPassword: {
+    marginBottom: 20, // Increase this value for more space between password and the email
+  },
+  fullWidth: {
+    width: "100%",
+  },
+  loginButton: {
+    backgroundColor: "#00BFFF",
+    padding: 10,
+    borderRadius: 20,
+    marginBottom: 3,
+  },
+  loginButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+  justifyCenter: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  signupText: {
+    color: "#00BFFF",
+  },
 });
---
+++ src/screens/RegisterScreen.js
@@ -0,0 +1,169 @@
+import React, {useState} from "react";
+import {
+  View,
+  Text,
+  Image,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useNavigation } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
+import db from "@react-native-firebase/database";
+
+export default function RegisterScreen() {
+    const navigation = useNavigation();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const createProfile = async (response) => {
+      // Ensure db() is correctly set up to interact with your database
+      db().ref(`/users/${response.user.uid}`).set({ name, email });
+    };
+  
+    const registerAndGoToMainFlow = async () => {
+      if (email && password) {
+        try {
+          const response = await auth().createUserWithEmailAndPassword(email, password);
+          if (response.user) {
+            await createProfile(response);
+            navigation.push("Home"); // Use the correct navigation method
+          }
+        } catch (e) {
+          // It's better to show more specific error messages based on e.code or e.message
+          Alert.alert("Registration Error", e.message || "Please check your form and try again");
+        }
+      }
+    };
+  
+    return (
+      <View style={styles.container}>
+        <StatusBar style="light" />
+        <Image style={styles.backgroundImage} source={require("../assets/backgroundimage.jpeg")} />
+  
+        <View style={styles.titleAndFormContainer}>
+          <View style={styles.titleContainer}>
+            <Text style={styles.titleText}>Register</Text> 
+          </View>
+  
+          <View style={styles.formContainer}>
+            <View style={[styles.inputContainer, styles.mbEmail]}>
+              <TextInput
+                placeholder="Name"
+                placeholderTextColor={"gray"}
+                value={name}
+                onChangeText={setName} // Bind state setter
+              />
+            </View>
+            <View style={[styles.inputContainer, styles.mbEmail]}>
+              <TextInput
+                placeholder="Email"
+                placeholderTextColor={"gray"}
+                value={email}
+                onChangeText={setEmail} // Bind state setter
+              />
+            </View>
+            <View style={[styles.inputContainer, styles.mbPassword]}>
+              <TextInput
+                placeholder="Password"
+                placeholderTextColor={"gray"}
+                // secureTextEntry
+                value={password}
+                onChangeText={setPassword} // Bind state setter
+              />
+            </View>
+  
+            <View style={styles.fullWidth}>
+              <TouchableOpacity
+                style={styles.loginButton}
+                onPress={registerAndGoToMainFlow} // Changed the onPress handler
+              >
+                <Text style={styles.loginButtonText}>Register</Text>
+              </TouchableOpacity>
+            </View>
+  
+            <View style={styles.justifyCenter}>
+              <Text>Already have an account? </Text>
+              <TouchableOpacity onPress={() => navigation.push("LogIn")}>
+                <Text style={styles.signupText}>Login</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    );
+  };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  backgroundImage: {
+    ...StyleSheet.absoluteFillObject,
+    height: undefined,
+    width: undefined,
+    flex: 1,
+  },
+  titleAndFormContainer: {
+    flex: 1,
+    justifyContent: "space-around",
+    paddingTop: 40,
+    paddingBottom: 10,
+  },
+  titleContainer: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  titleText: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 28,
+    letterSpacing: 1,
+  },
+  formContainer: {
+    flex: 1,
+    alignItems: "center",
+    marginHorizontal: 20, // Increase the margin for more space on each side
+    marginBottom: 10,
+  },
+  inputContainer: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    padding: 10,
+    borderRadius: 20,
+    width: "100%",
+  },
+  mbEmail: {
+    marginBottom: 20, // Increase this value for more space between email and the top
+  },
+  mbPassword: {
+    marginBottom: 20, // Increase this value for more space between password and the email
+  },
+  fullWidth: {
+    width: "100%",
+  },
+  loginButton: {
+    backgroundColor: "#00BFFF",
+    padding: 10,
+    borderRadius: 20,
+    marginBottom: 3,
+  },
+  loginButtonText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+  justifyCenter: {
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  signupText: {
+    color: "#00BFFF",
+  },
+});
