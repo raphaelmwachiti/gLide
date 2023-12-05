c5e054e979f6337f51f106aa49f0c648e70db0c7
ammaarkhan <ammaarkhan03@gmail.com>
2023-12-05 12:52:59 -0800

    update ride history

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,127 +1,116 @@
-import React from 'react';
-import { View, Text, ScrollView, TouchableOpacity, StyleSheet, TextInput, Image } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  ScrollView,
+  TouchableOpacity,
+  StyleSheet,
+  TextInput,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const ChatScreen = ({ navigation }) => {
-  // Placeholder for message state and any other logic (not implemented)
+  const [messages, setMessages] = useState([
+    "Hey I should be by the elephant wearing blue nike fit",
+    "I arrive in 5 minutes",
+    "I don't see you",
+    "Glide completed (chat deletion - 10:03)",
+    "You left your socks",
+  ]);
+  const [newMessage, setNewMessage] = useState("");
+
+  const sendMessage = () => {
+    if (newMessage.trim().length > 0) {
+      setMessages([...messages, newMessage]);
+      setNewMessage("");
+      // Here you would also handle sending the message to your backend or Firebase
+    }
+  };
 
   return (
-    <View style={styles.container}>
-        <View style={{flexDirection: 'row', backgroundColor: '#f2f2f2'}}> 
-            <TouchableOpacity style={styles.backButton} onPress={() => navigation.navigate('TrackRideScreen')}>
-                <Text style={styles.buttonText}>Back</Text>
-            </TouchableOpacity>
-            <Text style={styles.deletionNotice}>Chat is deleted 30 minutes after ride</Text>
-        </View>      
-      <ScrollView style={styles.chatContainer}>
-        
-        <Text style={styles.message}>Hey I should be by the elephant wearing blue nike fit</Text>
-        <Text style={styles.message}>I arrive in 5 minutes</Text>
-        <Text style={styles.message}>I don't see you</Text>
-        <Text style={styles.message}>Glide completed (chat deletion - 10:03)</Text>
-        <Text style={styles.message}>You left your socks</Text>
-        
-      </ScrollView>
-
-      <View style={styles.quickResponseContainer}>
-        <TouchableOpacity style={styles.quickResponseButton}>
-          <Text>At Gordon</Text>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={24} color="black" />
         </TouchableOpacity>
-        <TouchableOpacity style={styles.quickResponseButton}>
-          <Text>Arriving in (5:07)</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.quickResponseButton}>
-          <Image source={require('../assets/phone-icon.png')} style={styles.callIcon} />
-        </TouchableOpacity>
+        <Text style={styles.deletionNotice}>
+          Chat is deleted 30 minutes after ride
+        </Text>
       </View>
 
-      <Text style={styles.disabledChatNotice}>
-        You are driving, chat is disabled. Use suggestions above
-      </Text>
+      <ScrollView style={styles.chatContainer}>
+        {/* Messages would be dynamically mapped here */}
+      </ScrollView>
 
-      <TextInput 
-        style={styles.chatInput} 
-        placeholder="Chat with Glider" 
-        editable={false} 
-      />
-    </View>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Chat with Glider"
+          value={newMessage}
+          onChangeText={setNewMessage}
+          onSubmitEditing={sendMessage} // This will send the message when the return key is pressed
+          returnKeyType="send" // Just for better UX on iOS
+          editable={true} // Can be set to false if you want to disable input
+        />
+        <TouchableOpacity style={styles.sendButton} onPress={sendMessage}>
+          <Ionicons name="send" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    marginTop: 50,
-    
+    backgroundColor: "white",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#f2f2f2",
+    backgroundColor: "white",
   },
   deletionNotice: {
-    padding: 10,
-    backgroundColor: '#f2f2f2',
-    color: '#f54e42', 
-    fontWeight: 'bold',
-    textAlign: 'center',
-    fontSize: 13,
-    marginTop: 10
+    marginLeft: 10,
+    color: "#f54e42",
+    fontSize: 14,
   },
   chatContainer: {
     flex: 1,
     padding: 10,
   },
-  message: {
-    padding: 8,
-    backgroundColor: '#e5e5e5',
-    borderRadius: 15,
-    marginBottom: 10,
-    maxWidth: '80%',
-    alignSelf: 'flex-start',
-  },
-  quickResponseContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
     padding: 10,
-    backgroundColor: '#f2f2f2',
+    borderTopWidth: 1,
+    borderTopColor: "#f2f2f2",
   },
-  quickResponseButton: {
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: "#dedede",
     padding: 10,
-    backgroundColor: '#ddd',
+    marginRight: 10,
+    borderRadius: 20,
+    backgroundColor: "#f2f2f2",
+    color: "black",
+  },
+  sendButton: {
+    backgroundColor: "#1e90ff",
+    padding: 10,
     borderRadius: 20,
   },
-  callIcon: {
-    width: 20, // Adjust size as needed
-    height: 20, // Adjust size as needed
-    resizeMode: 'contain',
-  },
-  disabledChatNotice: {
-    padding: 10,
-    backgroundColor: '#f2f2f2',
-    textAlign: 'center',
-    fontSize: 12,
-  },
-  chatInput: {
-    padding: 10,
-    backgroundColor: '#e5e5e5',
-    fontSize: 14,
-    borderTopWidth: 1,
-    borderColor: '#ddd',
-    marginBottom: 25,
-    marginHorizontal: 10,
-    borderRadius: 15, 
-  },
-  buttonText: {
-    color: '#fff',
-    textAlign: 'center',
-    fontWeight: 'bold',
-  }, 
-  backButton: {
-    backgroundColor: '#000', 
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 5,
-    marginTop: 10, 
-    marginRight: 10, 
-    marginLeft: 5, 
-    marginBottom: 10, 
-  }, 
 });
 
 export default ChatScreen;
