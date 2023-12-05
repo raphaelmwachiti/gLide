c6db88c0de9b92a0edb683eece9f545373de4cf6
raphaelmwachiti <raphaelmwachiti@gmail.com>
2023-12-05 12:32:28 -0800

    c

--- src/components/CustomTabBar.js
+++ src/components/CustomTabBar.js
@@ -62,7 +62,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#DDD',
     borderRadius: 5,
-    marginTop: 60, 
+    marginTop: 20, 
     backgroundColor: '#DDD'
     
   },
