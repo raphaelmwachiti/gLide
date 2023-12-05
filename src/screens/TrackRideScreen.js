a04b1e273c055a835b78b6b827a912d48dfe4710
Raghav Bhagria <YOUR@EMAIL.com>
2023-12-05 10:05:09 -0800

    SafetyScreen Updated

--- src/screens/TrackRideScreen.js
+++ src/screens/TrackRideScreen.js
@@ -3,61 +3,67 @@ import { View, Text, Image, TouchableOpacity, StyleSheet, SafeAreaView } from 'r
 import MapView from 'react-native-maps';
 import SafetyScreen from './SafetyScreen';
 
+import FindRide from './findRide';
+
 const TrackRideScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        {/* Home button within SafeAreaView */}
+        <SafeAreaView style={styles.homeButtonSafeArea}>
+          <TouchableOpacity style={styles.homeButton} onPress={() => navigation.navigate(FindRide)}>
+            <Text style={styles.homeButtonText}>Home</Text>
+          </TouchableOpacity>
+        </SafeAreaView>
+
         <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
 
-      />
-
-      <View style={styles.card}>
-        <Text style={styles.titleText}> Ride details</Text>
-        <View style={styles.profileInfo}> 
-          <View style={styles.profileImgClmn}>
-            <Image
-            source={require('../assets/pp.jpg')} 
-            style={styles.profileImage}
-            />
+        <View style={styles.card}>
+          <Text style={styles.titleText}>Ride details</Text>
+          <View style={styles.profileInfo}>
+            <View style={styles.profileImgClmn}>
+              <Image source={require('../assets/pp.jpg')} style={styles.profileImage} />
+            </View>
+            <View style={styles.profileDetailClmn}>
+              <Text style={styles.profileName}>Lambo Urus - 5.767$</Text>
+              <Text style={styles.profileDetail}>Messi Leo Goat</Text>
+              <Text style={styles.profileDetail}>(add star emo) 2/7 - silver</Text>
+            </View>
           </View>
-          <View style={styles.profileDetailClmn}> 
-            <Text style={styles.profileName}>Lambo Urus - 5.767$</Text>
-            <Text style={styles.profileDetail}>Messi Leo Goat</Text>
-            <Text style={styles.profileDetail}>(add star emo) 2/7 - silver</Text>
+
+          <Text style={styles.aboutTitle}>Live updates</Text>
+          <Text style={styles.aboutDetail}>- Appears to be heavy traffic</Text>
+          <Text style={styles.aboutDetail}>- Messi is rerouting</Text>
+          <Text style={styles.aboutDetail}>- The plate number is 345GGf </Text>
+          <Text style={styles.aboutDetail}>- His car is bright Pink and should have green rims </Text>
+          <Text style={styles.aboutDetail}>- Be mindful of your ride </Text>
+          <Text style={styles.aboutDetail}>- Call 911 us you feel unsafe and contact support</Text>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.cancelButton} onPress={() => navigation.navigate('GlideDriveTabs', { screen: 'Glide' })}>
+              <Text style={styles.buttonText}>Cancel Glide (5:03)</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.trackButton} onPress={() => navigation.navigate('ChatScreen')}>
+              <Text style={styles.buttonText}>Talk to driver</Text>
+            </TouchableOpacity>
           </View>
-        </View>
 
-        <Text style={styles.aboutTitle}>Live updates</Text>
-        <Text style={styles.aboutDetail}> - Appears to be heavy traffic</Text>
-        <Text style={styles.aboutDetail}> - Messi is rerouting</Text>
-        <Text style={styles.aboutDetail}> - The plate number is 345GGf </Text>
-        <Text style={styles.aboutDetail}> - His car is bright Pink and should have green rims </Text>
-        <Text style={styles.aboutDetail}> - Be mindful of your ride </Text>
-        <Text style={styles.aboutDetail}> - Call 911 us you feel unsafe and contact support</Text>
-       
-
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.cancelButton} onPress={() => navigation.navigate('GlideDriveTabs', { screen: 'Glide' })}>
-            <Text style={styles.buttonText}> Cancel Glide (5:03)</Text>
-          </TouchableOpacity>
-          <TouchableOpacity style={styles.trackButton} onPress={() => navigation.navigate('ChatScreen')}>
-            <Text style={styles.buttonText}> Talk to driver </Text>
-          </TouchableOpacity>
-        </View> 
-
-        <TouchableOpacity style={styles.shareButton} onPress={() => navigation.navigate('GlideDriveTabs', { screen: 'Glide' })}>
+          <TouchableOpacity style={styles.shareButton} onPress={() => navigation.navigate('GlideDriveTabs', { screen: 'Glide' })}>
             <Text style={styles.buttonText}>Share live location and ride details</Text>
           </TouchableOpacity>
 
-        <TouchableOpacity style={styles.safetyTipsButton} onPress={()=> navigation.navigate(SafetyScreen)}>
-          <Text style={styles.safetyTipsText}>Safety tips</Text>
-        </TouchableOpacity>
+          <TouchableOpacity style={styles.safetyTipsButton} onPress={() => navigation.navigate(SafetyScreen)}>
+            <Text style={styles.safetyTipsText}>Safety tips</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -66,60 +72,68 @@ const TrackRideScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#fff', 
-  }, 
+  },
   map: {
-    height: 250, 
+    height: 250,
     width: '100%',
-    marginBottom: 20, 
+    marginBottom: 20,
   },
   card: {
-    backgroundColor: '#f0f0f0', 
+    backgroundColor: '#f0f0f0',
     borderRadius: 8,
     padding: 20,
-    alignItems: 'center', 
+    alignItems: 'center',
     width: '90%',
-    maxWidth: 600, 
+    maxWidth: 600,
+    position: 'relative',
   },
-  confirmationImage: {
-    width: 100,
-    height: 100,
-    borderRadius: 60,
+  homeButtonSafeArea: {
+    position: 'absolute',
+    top: 0,
+    left: 10,
+    right: 10,
+    zIndex: 1,
+  },
+  homeButton: {
+    position: 'absolute',
+    top: 5,
+    left: 5,
+    zIndex: 1,
+  },
+  homeButtonText: {
+    fontSize: 16,
+    color: '#333',
+  },
+  titleText: {
     marginBottom: 10,
-  },
-  confirmationText: {
-    marginBottom: 20,
     textAlign: 'center',
     color: '#333',
-    fontSize: 20,
     fontWeight: 'bold',
-  }, 
-  titleText: { 
-    marginBottom: 10,
-    textAlign: 'center',
-    color: '#333',
-    fontWeight: 'bold',
-    textDecorationLine: 'underline'
+    textDecorationLine: 'underline',
   },
   profileInfo: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    marginHorizontal: 6, 
+    marginHorizontal: 6,
   },
-  profileImgClmn: { 
-    width: '20%'
-  }, 
+  profileImgClmn: {
+    width: '20%',
+  },
   profileDetailClmn: {
-    width: '70%'
+    width: '70%',
   },
   profileImage: {
     width: 70,
     height: 70,
     borderRadius: 60,
-    marginLeft: -20
+    marginLeft: -20,
   },
   profileName: {
     fontWeight: 'bold',
@@ -148,41 +162,38 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
   },
   cancelButton: {
-    backgroundColor: '#000', 
+    backgroundColor: '#000',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
-    marginTop: 10, 
-    marginRight: 20, 
-  }, 
+    marginTop: 10,
+  },
   shareButton: {
-    backgroundColor: '#000', 
+    backgroundColor: '#000',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
-    marginTop: 10, 
-    marginRight: 10, 
+    marginTop: 10,
   },
   trackButton: {
-    backgroundColor: '#21d111', 
+    backgroundColor: '#21d111',
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 5,
-    marginTop: 10, 
+    marginTop: 10,
   },
   buttonText: {
     color: '#fff',
     textAlign: 'center',
     fontWeight: 'bold',
-  }, 
+  },
   safetyTipsButton: {
-    marginTop:10,
+    marginTop: 10,
   },
   safetyTipsText: {
     fontWeight: 'bold',
-    color: '#21d111', 
+    color: '#21d111',
   },
-  
 });
 
 export default TrackRideScreen;
